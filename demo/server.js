9ec1ad46c663bd0358f6587c4ca3e7dc38f4a82b
Jafar Akhondali <jafar.akhoondali@gmail.com>
2024-07-30 18:43:57 +0200

    Block malicious looking requests to prevent path traversal attacks.

--- demo/server.js
+++ demo/server.js
@@ -5,6 +5,11 @@ http = require('http');
 const SERVER_PORT = 8899;
 
 http.createServer(function (req, res) {
+    if (path.normalize(decodeURI(req.url)) !== decodeURI(req.url)) {
+        res.statusCode = 403;
+        res.end();
+        return;
+    }
     let filename = path.join(__dirname, req.url);
     if (req.url != '/index.html') {
         filename = path.join(__dirname, '../', req.url)
